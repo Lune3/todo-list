03519532b3e618626551ee620cd3e6aec449fc26
Lune3 <113820374+Lune3@users.noreply.github.com>
2024-04-04 17:11:26 +0530

    Made create Project static member of project

--- src/project.js
+++ src/project.js
@@ -1,7 +1,5 @@
 import {createP,createHeading,createImg} from './create';
 
-const projects = []
-
 class project{
     static projectCounter = 0;
     constructor(projectName){
@@ -12,6 +10,15 @@ class project{
         this.projectCounter++;
         return this.projectCounter;
     }
+
+    static projectCreate(projectName){
+        const projectList = document.querySelector(".projectList");
+        const li = document.createElement('li');
+        li.className = `p${project.counterIncrement()}`;
+        li.textContent = `${projectName}`;
+        li.append(createImg('https://www.svgrepo.com/show/463489/delete-alt.svg'));
+        projectList.append(li);
+    }
 }
 
 function projectDialogHandler(){
@@ -20,23 +27,17 @@ function projectDialogHandler(){
     
     const projectSubmitButton = document.querySelector("form > button");
     const projectInput = document.querySelector("input");
-
-    projectSubmitButton.addEventListener("click" , () => { 
-        projectCreate(projectInput.value);
-        projectDialog.close();
-
+    
+    projectSubmitButton.addEventListener("click" , (e) => { 
+        e.preventDefault();
+        document.querySelector("form").checkValidity();
+        if(document.querySelector("form").reportValidity()){
+            project.projectCreate(projectInput.value);
+            projectInput.value = "";
+            projectDialog.close();
+        }
     },{once : true});
-
-
 }
 
-function projectCreate(projectName){
-    const projectList = document.querySelector(".projectList");
-    const li = document.createElement('li');
-    li.className = `p${project.counterIncrement()}`;
-    li.textContent = `${projectName}`;
-    li.append(createImg('https://www.svgrepo.com/show/463489/delete-alt.svg'));
-    projectList.append(li);
-}
 
 export {projectDialogHandler};
